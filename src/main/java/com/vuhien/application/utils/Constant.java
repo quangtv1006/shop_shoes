2caf8caabdfc26bb46401d767cd32d722385027d
Trương Văn Quang <quangtv1006@gmail.com>
2022-06-27 21:37:09 +0700

    first commit

---
+++ src/main/java/com/vuhien/application/utils/Constant.java
@@ -0,0 +1,6 @@
+package com.vuhien.application.utils;
+
+public class Constant {
+
+  public static final String USER = "USER";
+}
---
+++ src/main/java/com/vuhien/application/utils/Message.java
@@ -0,0 +1,9 @@
+package com.vuhien.application.utils;
+
+public class Message {
+  public static class ErrorMessage {
+    public static final String EMAIL_PASS_INCORRECT = "Email hoặc mật khẩu không chính xác!";
+    public static final String CHANGE_PASSWORD_SUCCESS = "Đổi mật khẩu thành công";
+    public static final String UPDATE_SUCCESS = "Cập nhật thành công";
+  }
+}
